1ccb7d5818da9b3f9740d4441de2c211a4e64e55
sangpham27 <bsangb@outlook.com>
2023-09-23 21:49:04 +0700

    init project

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1 style='text-align: center; font-size: 50px;'>Hello World, my name is SangPT4</h1>"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
